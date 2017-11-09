59e2d9f75837e6a682487ccc15b9cccfc8044859
Nicolas Six <nicolasix@outlook.fr>
2017-11-09 11:56:24 +1100

    working stickman and level

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -1,11 +1,51 @@
 import sys, random
 import pygame
 from pygame.locals import *
+from pymunk import Vec2d
 import pymunk 
 import pymunk.pygame_util
 from sqlalchemy.sql.expression import false
+from array import array
+from math import pow
+from math import sqrt
 
 class Stickman:
+        
+    def genStick(self):
+        self.space.add(
+        self.corpse.body,
+        self.corpse.corpse_shape,
+        self.corpse.head_shape,
+        self.left_arm.body,
+        self.left_arm.left_arm_shape,
+        self.right_arm.body,
+        self.right_arm.right_arm_shape,
+        self.left_leg.body,
+        self.left_leg.left_leg_shape,
+        self.right_leg.body,
+        self.right_leg.right_leg_shape,
+        self.pin_la_c,
+        self.pin_ll_c,
+        self.pin_ra_c,
+        self.pin_rl_c
+    )
+        
+    def genJunctions(self):
+        self.pin_ra_c = pymunk.PinJoint(self.right_arm.body, self.corpse.body, (0,0), (0, 30*self.size))
+        self.pin_ra_c_r = pymunk.DampedRotarySpring(self.right_arm.body, self.right_arm.body_joint, -0.15, 20000000, 900000)
+        self.pin_ra_c.error_bias = pow(0.7, 60.0)      
+        
+        self.pin_la_c = pymunk.PinJoint(self.left_arm.body, self.corpse.body, (0,0), (0, 30*self.size))  
+        self.pin_la_c_r = pymunk.DampedRotarySpring(self.left_arm.body, self.left_arm.body_joint, -0.15, 20000000, 900000)
+        self.pin_la_c.error_bias = pow(0.7, 60.0)      
+        
+        self.pin_rl_c = pymunk.PinJoint(self.right_leg.body, self.corpse.body, (0,0), (0, 0))
+        self.pin_rl_c_r = pymunk.DampedRotarySpring(self.right_leg.body, self.right_leg.body_joint, -0.15, 20000000, 900000)
+        self.pin_rl_c.error_bias = pow(0.7, 60.0)  
+        
+        self.pin_ll_c = pymunk.PinJoint(self.left_leg.body, self.corpse.body, (0,0), (0, 0))
+        self.pin_ll_c_r = pymunk.DampedRotarySpring(self.left_leg.body, self.left_leg.body_joint, 10, 1, 1)
+        self.pin_ll_c.error_bias = pow(0.7, 60.0)  
     
     def __init__(self, size, x, y, mass, moment, friction, space):
         self.mass = mass
@@ -20,57 +60,18 @@ class Stickman:
         
         self.genJunctions()
         self.genStick()
-        
-    def genStick(self):
-        self.space.add(
-            self.corpse.body,
-            self.corpse.corpse_shape,
-            self.corpse.head_shape,
-            self.left_arm.body,
-            self.left_arm.left_arm_shape,
-            self.right_arm.body,
-            self.right_arm.right_arm_shape,
-            self.left_leg.body,
-            self.left_leg.left_leg_shape,
-            self.right_leg.body,
-            self.right_leg.right_leg_shape,
-            self.pin_la_c,
-            self.pin_ll_c,
-            self.pin_ra_c,
-            self.pin_rl_c
-        )
-        
-    def genJunctions(self):
-        self.pin_ra_c = pymunk.PinJoint(self.right_arm.body, self.corpse.body, (-10*self.size, 20*self.size), (0, 30*self.size))
-        self.pin_ra_c.error_bias = pow(0.7, 60.0)      
-        
-        self.pin_la_c = pymunk.PinJoint(self.left_arm.body, self.corpse.body, (10*self.size, 20*self.size), (0, 30*self.size))  
-        self.pin_la_c.error_bias = pow(0.7, 60.0)      
-        
-        self.pin_rl_c = pymunk.PinJoint(self.right_leg.body, self.corpse.body, (-10*self.size, 20*self.size), (0, 0))
-        self.pin_rl_c.error_bias = pow(0.7, 60.0)  
-        
-        self.pin_ll_c = pymunk.PinJoint(self.left_leg.body, self.corpse.body, (10*self.size, 20*self.size), (0, 0))
-        self.pin_ll_c.error_bias = pow(0.7, 60.0)  
-        
-        
-    def stickImpulse(self):
-        self.left_leg.body.apply_impulse_at_local_point((0,50), (self.left_leg.body.position))
-        self.right_leg.body.apply_impulse_at_local_point((0,50), (self.right_leg.body.position))
+
         
 class Member:
-    
-    def __init__(self, mass, moment, x, y, friction):
-        self.x = x 
-        self.y = y
+    def __init__(self, mass, moment, friction):
         self.body = pymunk.Body(mass, moment)
+        self.body_joint = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
         self.friction = friction
-
-
-class Corpse(Member) : 
+        
+class Corpse() : 
     
-    def __init__(self, size, x, y, mass, moment, friction):
-        Member.__init__(self, mass, moment, x, y , friction)
+    def __init__(self, size, x, y, mass, moment, friction):  
+        self.body = pymunk.Body(mass, moment)
         self.body.position = (x,y - 40*size)
         
         self.head_shape = pymunk.Circle(self.body, 10*size, (0,38*size))
@@ -78,17 +79,18 @@ class Corpse(Member) :
         self.head_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)  
    
         self.corpse_shape = pymunk.Segment(self.body, (0, 0), (0, 30*size), 2*size)
-        self.corpse_shape.friction = 0.2
+        self.corpse_shape.friction = friction
         self.corpse_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)
 
       
 class LeftLeg(Member) : 
     
     def __init__(self, size, x, y, mass, moment, friction):
-        Member.__init__(self, mass, moment, x, y , friction)
-        self.body.position =(x - 10*size, y - 60*size)
+        Member.__init__(self, mass, moment, friction)
+        self.body.position =(x, y - 40*size)
+        self.body_joint.position = self.body.position
         
-        self.left_leg_shape = pymunk.Segment(self.body, (10*size, 20*size), (0,0), 2*size)
+        self.left_leg_shape = pymunk.Segment(self.body, (-10*size, -20*size), (0,0), 2*size)
         self.left_leg_shape.friction = friction
         self.left_leg_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)  
 
@@ -96,10 +98,11 @@ class LeftLeg(Member) :
 class RightLeg(Member) : 
     
     def __init__(self, size, x, y, mass, moment, friction):
-        Member.__init__(self, mass, moment, x, y , friction)
-        self.body.position =(x + 10*size, y - 60*size)
+        Member.__init__(self, mass, moment, friction)
+        self.body.position =(x, y - 40*size)
+        self.body_joint.position = self.body.position
         
-        self.right_leg_shape = pymunk.Segment(self.body, (-10*size, 20*size), (0, 0), 2*size)
+        self.right_leg_shape = pymunk.Segment(self.body, (10*size, -20*size), (0, 0), 2*size)
         self.right_leg_shape.friction = friction
         self.right_leg_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)  
 
@@ -107,10 +110,11 @@ class RightLeg(Member) :
 class LeftArm(Member) : 
     
     def __init__(self, size, x, y, mass, moment, friction):
-        Member.__init__(self, mass, moment, x, y , friction)
-        self.body.position =(x - 10*size, y - 30*size)
+        Member.__init__(self, mass, moment , friction)
+        self.body.position =(x, y - 10*size)
+        self.body_joint.position = self.body.position
         
-        self.left_arm_shape = pymunk.Segment(self.body, (10*size, 20*size), (0, 0), 2*size)
+        self.left_arm_shape = pymunk.Segment(self.body, (-10*size, -20*size), (0, 0), 2*size)
         self.left_arm_shape.friction = friction
         self.left_arm_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)  
 
@@ -118,10 +122,11 @@ class LeftArm(Member) :
 class RightArm(Member) : 
     
     def __init__(self, size, x, y, mass, moment, friction):
-        Member.__init__(self, mass, moment, x, y , friction)
-        self.body.position =(x + 10*size, y - 30*size)
+        Member.__init__(self, mass, moment , friction)
+        self.body.position =(x, y - 10*size)
+        self.body_joint.position = self.body.position
         
-        self.right_arm_shape = pymunk.Segment(self.body, (-10*size, 20*size), (0, 0), 2*size)
+        self.right_arm_shape = pymunk.Segment(self.body, (10*size, -20*size), (0, 0), 2*size)
         self.right_arm_shape.friction = friction
         self.right_arm_shape.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)  
 
@@ -136,3 +141,81 @@ class Environment():
         self.environment.color = color
         self.space = space
         space.add(self.environment, self.body)
+        
+class Level():
+    def generateLevelGrid(self):
+        self.w, self.h = int(self.x_resolution/20), int(self.y_resolution/20);
+        return [[0 for x in range(self.h)] for y in range(self.w)] 
+        
+
+    def generateBounds(self):
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = (0,0)
+        segLeft = pymunk.Segment(body, (0,0), (0,self.y_resolution), 2)
+        segRight = pymunk.Segment(body, (self.x_resolution,0), (self.x_resolution,self.y_resolution), 2)
+        segTop = pymunk.Segment(body, (0,self.y_resolution), (self.x_resolution,self.y_resolution), 2)
+        segBot = pymunk.Segment(body, (0,0), (self.x_resolution,0), 2)
+        self.space.add(body, segLeft, segBot, segRight, segTop)        
+    
+    def generateLevel(self):
+        self.generateBounds()
+        self.obstaclesGrid[5][2] = 1
+        self.obstaclesGrid[6][2] = 1
+        self.obstaclesGrid[9][3] = 1
+        self.obstaclesGrid[10][3] = 1
+        self.obstaclesGrid[14][1] = 1
+        self.obstaclesGrid[15][2] = 1
+        self.obstaclesGrid[16][2] = 1
+        self.obstaclesGrid[17][3] = 1
+        self.obstaclesGrid[18][3] = 1
+        self.obstaclesGrid[19][3] = 1
+        
+        self.obstaclesGrid[19][4] = 2
+        print("Level Generated")
+        
+    def renderLevel(self):
+        for i in range(self.w):
+            for j in range(self.h):
+                if self.obstaclesGrid[i][j] == 1:
+                    self.addObstacle(i,j)
+                    
+                elif self.obstaclesGrid[i][j] == 2:
+                    self.addObjectivePoint(i,j)
+    
+    def addObstacle(self, i, j):
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = (i*int(self.x_resolution/20), j*int(self.y_resolution/20))
+        
+        segLeft = pymunk.Segment(body, (0,0), (0,self.y_resolution/20), 1)
+        segRight = pymunk.Segment(body, (self.x_resolution/20,0), (self.x_resolution/20,self.y_resolution/20), 1)
+        segTop = pymunk.Segment(body, (0,self.y_resolution/20), (self.x_resolution/20,self.y_resolution/20), 1)
+        segBot = pymunk.Segment(body, (0,0), (self.x_resolution/20,0), 1)
+        
+        segLeft.color, segRight.color, segTop.color, segBot.color = ((0,0,0),(0,0,0),(0,0,0),(0,0,0))
+        self.space.add(body, segLeft, segBot, segRight, segTop)
+        
+    def addObjectivePoint(self,i,j):
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = (i*int(self.x_resolution/20) + int(self.x_resolution/40), j*int(self.y_resolution/20) + int(self.y_resolution/20))
+        objective = pymunk.Circle(body, 10, (0,0))
+        objective.color = 0,200,0
+        objective.filter = pymunk.ShapeFilter(categories=0x1, mask=pymunk.ShapeFilter.ALL_MASKS ^ 0x1)
+        self.objectivePointBody = body
+        self.space.add(body, objective)
+        
+    def __init__(self, x_resolution, y_resolution, space):
+        self.x_resolution = x_resolution
+        self.y_resolution = y_resolution
+        self.space = space
+        self.obstaclesGrid = self.generateLevelGrid()
+        self.generateLevel()
+        self.renderLevel()
+        
+class Util():
+    def distanceBetweenTwoBodies(firstBody, secondBody):
+        (x_a,y_a) = firstBody.position
+        (x_b,y_b) = secondBody.position
+        return sqrt(pow(x_b - x_a, 2) + pow(y_b - y_a, 2))
+    
+    def __init(self):
+        pass
--- main.py
+++ main.py
@@ -3,42 +3,64 @@ import pygame
 from pygame.locals import *
 import pymunk 
 import pymunk.pygame_util
+from pymunk import Vec2d
 from sqlalchemy.sql.expression import false
 from graphics import graphics
 
 def main():
     pygame.init()
-    x_resolution = 1000
+    x_resolution = 1200
     y_resolution = 700
     screen = pygame.display.set_mode((x_resolution, y_resolution))
     pygame.display.set_caption("Test géométrie personnage")
     clock = pygame.time.Clock()
+    size = 1.5
     friction = 0.2
+    mass = 2
+    moment = 50000
     color_grass = 34,139,34
     space = pymunk.Space()
     space.gravity = (0.0, -900.0)
+    original_stick = graphics.Stickman(size, x_resolution/7, y_resolution/3, mass, moment, friction, space)
+    level = graphics.Level(x_resolution, y_resolution, space)
     env = graphics.Environment(friction, color_grass, space, x_resolution)
-    original_stick = graphics.Stickman(5, x_resolution/2, y_resolution/1.5, 10, 2000, friction, space)
     draw_options = pymunk.pygame_util.DrawOptions(screen)
     
-    ticks_to_next_stick = 10
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit(0)
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 sys.exit(0)
+              
+            #test angle case  
+            elif event.type == KEYDOWN and event.key == K_UP:
+                print("Déplacement jambe droite")
+                original_stick.right_leg.body.angle += 1.57
+                space.reindex_shape(original_stick.right_leg.right_leg_shape)
+
+            elif event.type == KEYDOWN and event.key == K_DOWN:
+                print("Déplacement jambe gauche")
+                original_stick.left_leg.body.angle += 1.57
+                space.reindex_shape(original_stick.left_leg.left_leg_shape)
                 
-            elif event.type == MOUSEBUTTONUP:
-                x,y = pygame.mouse.get_pos()
-                graphics.Stickman(5, x, y_resolution - y, 10, 2000, friction, space)
-
-        ticks_to_next_stick -= 1
-        if ticks_to_next_stick <= 0:
-            ticks_to_next_stick = 500
-            original_stick.stickImpulse()
-
-        screen.fill((255,255,240))
+            elif event.type == KEYDOWN and event.key == K_LEFT:
+                print("Déplacement bras gauche")
+                original_stick.left_arm.body.angle += 1.57
+                space.reindex_shape(original_stick.left_arm.left_arm_shape)
+                
+            elif event.type == KEYDOWN and event.key == K_RIGHT:
+                print("Déplacement bras droit")
+                original_stick.right_arm.body.angle += 1.57
+                space.reindex_shape(original_stick.right_arm.right_arm_shape)
+                
+            #elif event.type == MOUSEBUTTONUP:
+                #print("New stickman")
+                #x,y = pygame.mouse.get_pos()
+                #graphics.Stickman(size, x, y_resolution - y, mass, moment, friction, space)
+        
+        print("Distance : " + str(graphics.Util.distanceBetweenTwoBodies(original_stick.corpse.body,level.objectivePointBody)))
+        screen.fill((240, 255, 255))
 
         space.debug_draw(draw_options)
 
